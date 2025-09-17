f7d11a0634c5f33f66918a7064c2917d9451a4e1
mirkomornelli-commits <mirko.mornelli@awentia.com>
2025-09-17 21:18:57 +0200

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.get("/")
+def home():
+    return {"message": "Hello from Railway!"}
